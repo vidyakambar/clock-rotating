9975e67aaaadccc31e3555c367bffe90db159f58
vidya <vidyakambar18@gmail.com>
2024-06-22 12:20:43 +0530

    car moving

---
+++ Clock Using HTML, CSS & JS/index.js
@@ -0,0 +1,22 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+    
+    var audio = document.createElement('audio');
+
+    audio.setAttribute('src', 'song.mp3'); 
+
+    audio.loop=true;
+
+    audio.play();
+
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
